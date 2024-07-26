f53db149e165a5ee8bc8970d76fe5cbbe92f5313
Lauren <laurenyoo317@gmail.com>
2024-07-26 04:36:36 -0400

    Revoked old API key and created new env variables

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCtd2MKRlH3dgQb-mulbLJCYKHmb0IumF8",
-  authDomain: "study-app-a4cba.firebaseapp.com",
-  projectId: "study-app-a4cba",
-  storageBucket: "study-app-a4cba.appspot.com",
-  messagingSenderId: "536702443948",
-  appId: "1:536702443948:web:04909f2895a4e9c98bda09",
-  measurementId: "G-6EEDYL2ZY2",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
